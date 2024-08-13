95254cf449a651fb598b2c8a3685148826305a0c
Danny Rudnick <dannyrud@umich.edu>
2024-08-12 19:35:22 -0500

    first commit

---
+++ List/ListApp.swift
@@ -0,0 +1,20 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Daniel Rudnick on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
